3818227abf63aaca6ebd6c74ae5b2a9d1e18e536
Alison Kamila Reyes Baquero <alisonreyesuwu.com>
2025-11-18 09:53:40 -0500

    feat:Se realiza la creacion de un menu con 5 opciones, se habilita la opcion numero 1 que nos permite calcular el area del triangulo

--- main.py
+++ main.py
@@ -7,5 +7,18 @@ print("CALCULO AREA DE FIGURAS GEOMETRICAS")
 print("1. Triangulo ")
 print("2. Circulo ")
 print("3. Rectangulo ")
-print("4. Cuadrda")
+print("4. Cuadrado")
+print("5. Salir")
+
+opcion=int(input("Ingrese la opcion que desea usar: "))
+
+while True:
+    if opcion==1:
+        base=float(input("ingrese la medida de la base: "))
+        altura=float(input("ingrese la medida de la altura: "))
+        instancia=Triangulo(base,altura)
+        area=instancia.area()
+        print(area)
+
+
 
